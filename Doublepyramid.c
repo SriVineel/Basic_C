8de455a6cd947ea7d0db951b27f27957bd0758a9
SriVineel <srivineel39@gmail.com>
2024-10-11 17:10:23 +0530

    doublepyramid and hello committed.

---
+++ Doublepyramid.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int num,r,c1,c2;
+
+    printf("Enter the value of n:");
+    scanf("%d",&num);
+
+    for(r=1;r<=(2*num-1);r++)
+    {
+        for(c1=0;c1<=)
+    }
+}
